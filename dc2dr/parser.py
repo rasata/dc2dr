5540f7ee529266133fb5101eb83d216e684bd62e
x-malet <xavier.malet.1@ulaval.ca>
2019-05-19 15:09:36 -0400

    FIX: import ADD parse_volumes function

--- dc2dr/parser.py
+++ dc2dr/parser.py
@@ -1,72 +1,94 @@
 import yaml
-import sys
-import sorting
+from dc2dr.sorting import sort_service
+
 
 def run_commands(path):
     f = open(path)
     y = yaml.safe_load(f)
     return parse_compose_file(y)
 
+
 def parse_compose_file(yaml_file):
     services = yaml_file['services']
-    sorted_services = sorting.sort(services)
-
+    sorted_services = sort_service(services)
+    
     parsed_services = []
     for d in sorted_services:
         for k, v in d.items():
             parsed_services.append(parse_service(k, v))
-
+    
     commands = []
     for s in parsed_services:
         commands.append(write_run_command(s))
     return commands
+import pprint
 
 def parse_service(name, service):
     docker_args = {'name': name}
     docker_args['image'] = parse_image(service['image'])
-    for arg in ['depends_on', 'links', 'ports', 'expose', 'environment', 'command']:
+    for arg in ['depends_on', 'links', 'ports', 'expose', 'environment', 'command', 'volumes']:
         if arg in service:
             docker_args[arg] = globals()['parse_' + arg](service[arg])
     return docker_args
+
 
 def write_run_command(service):
     command = ""
     prefix = "docker run -d --name={0} ".format(service['name'])
     command += prefix
-    for arg in ['depends_on', 'links', 'ports', 'expose', 'environment']:
+    for arg in ['depends_on', 'links', 'ports', 'expose', 'environment', 'volumes']:
         if arg in service:
             command += service[arg]
     command += service['image']
     if 'command' in service:
-	    command += ' {0}'.format(service['command'])
+        command += ' {0}'.format(service['command'])
     return command
+
 
 def parse_image(image):
     return image
 
+
 def parse_depends_on(deps):
     return to_docker_arg(deps, " --link={0} ")
+
 
 def parse_links(links):
     return parse_depends_on(links)
 
+
 def parse_ports(ports):
     return to_docker_arg(ports, " -p {0} ")
+
 
 def parse_expose(exports):
     return to_docker_arg(exports, " --expose={0} ")
 
+
 def parse_environment(envs):
     string = ""
-    for k, v in envs.items():
-        string += ' -e {0}="{1}" '.format(k, v)
+    for elt in envs:
+        if isinstance(elt, str):
+            string += ' -e {} '.format(elt)
+        if isinstance(elt, dict):
+            for k, v in envs.items():
+                string += ' -e {0}="{1}" '.format(k, v)
     return string
+
+
+def parse_volumes(envs):
+    volumes = ""
+    for elt in envs:
+        volumes += ' -v {} '.format(elt)
+    return volumes
+
 
 def parse_command(command):
     if type(command) is list:
         return ' '.join(command)
     else:
         return command
+
 
 def to_docker_arg(args, str_format):
     string = ""
