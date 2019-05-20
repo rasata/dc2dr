24f74aa84f68f8cc76fbfb71e0c9cac80e859813
x-malet <xavier.malet.1@ulaval.ca>
2019-05-20 15:09:43 -0400

    remove unecessary imports

--- dc2dr/cli.py
+++ dc2dr/cli.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import click
-import yaml
-import sys
+
 from dc2dr import parser
 
 
 def parse_yml(path):
 
-	
 	run_commands = parser.DockerComposeFileParser(path).get_docker_run_commands()
 	for c in run_commands:
 		print(c)
