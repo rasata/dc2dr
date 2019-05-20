b4efeacab5ae184c4856441f70a0427ec1b13def
x-malet <xavier.malet.1@ulaval.ca>
2019-05-20 15:08:38 -0400

    update cli.py file

--- dc2dr/cli.py
+++ dc2dr/cli.py
@@ -5,18 +5,20 @@ import yaml
 import sys
 from dc2dr import parser
 
-def parse_yml(path):
-    f = open(path)
-    y = yaml.safe_load(f)
 
-    run_commands = parser.parse_compose_file(y)
-    for c in run_commands:
-        print(c)
+def parse_yml(path):
+
+	
+	run_commands = parser.DockerComposeFileParser(path).get_docker_run_commands()
+	for c in run_commands:
+		print(c)
+
 
 @click.command()
 @click.argument('f', type=click.Path(exists=True))
 def main(f):
-    parse_yml(f)
+	parse_yml(f)
+
 
 if __name__ == "__main__":
-    main()
+	main()
