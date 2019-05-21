1a1562e08805f3fa1b39f4f54e7f2a9ec482e1a7
x-malet <xavier.malet.1@ulaval.ca>
2019-05-21 14:17:58 -0400

    Parse envi files: 1- add possibility to parse an envi file. 2- find an envi file if absent (same place as docker-compose.yml) 3- replace env var in docker run commands

--- dc2dr/parser.py
+++ dc2dr/parser.py
@@ -1,3 +1,7 @@
+import os
+import re
+import warnings
+
 import yaml
 
 from dc2dr.sorting import sort_service
@@ -5,18 +9,44 @@ from dc2dr.sorting import sort_service
 
 class DockerComposeFileParser(object):
 	
-	def __init__(self, docker_compose_path: str) -> None:
+	def __init__(self, docker_compose_path: str, envi_file_location: str = None) -> None:
 		
 		self._dc_path = docker_compose_path
+		self._env_var_in_envi_file = {}
+		self._get_envi_file(envi_file_location)
+		
 		self._docker_file = open(self._dc_path)
 		self._dc_yaml_file = yaml.safe_load(self._docker_file)
-		self.parsers = {'depends_on': self._parse_depends_on,
-		                'links': self._parse_links,
-		                'ports': self._parse_ports,
-		                'expose': self._parse_expose,
-		                'environment': self._parse_environment,
-		                'command': self._parse_command,
-		                'volumes': self._parse_volumes}
+		self._parsers = {'depends_on': self._parse_depends_on,
+		                 'links': self._parse_links,
+		                 'ports': self._parse_ports,
+		                 'expose': self._parse_expose,
+		                 'environment': self._parse_environment,
+		                 'command': self._parse_command,
+		                 'volumes': self._parse_volumes,
+		                 'env_file': self._parse_envi_file}
+	
+	def _get_envi_file(self, envi_file_location):
+		envi_path = None
+		if envi_file_location is None:
+			dc_root = os.path.split(self._dc_path)[0]
+			if '.env' in os.listdir(dc_root):
+				envi_path = os.path.join(dc_root, '.env')
+				warn_string = "No .env file passed. {} is used as a default envi file".format(envi_file_location)
+				warnings.warn(message=warn_string)
+		else:
+			path, file = os.path.split(envi_file_location)
+			envi_path = os.path.join(path, file)
+		
+		if envi_path is None:
+			warnings.warn("No envi file provided")
+			return
+		else:
+			with open(envi_path, 'r') as envi_file:
+				for i in envi_file.readlines():
+					if re.match('.*=.*\n', i):
+						env_string = i.split("=")
+						self._env_var_in_envi_file[env_string[0]] = env_string[1].replace('\n', '')
 	
 	def get_docker_run_commands(self) -> list:
 		"""
@@ -56,23 +86,38 @@ class DockerComposeFileParser(object):
 		command = ""
 		prefix = "docker run -d --name={0} ".format(service['name'])
 		command += prefix
-		for arg in ['depends_on', 'links', 'ports', 'expose', 'environment', 'volumes']:
+		services = list(self._parsers.keys())
+		services.remove('command')
+		for arg in services:
 			if arg in service:
 				command += service[arg]
 		command += service['image']
 		if 'command' in service:
 			command += ' {0}'.format(service['command'])
+		self._replace_environnement_vars(command)
 		return command
+	
+	def _replace_environnement_vars(self, command_line: str):
+		if len(self._env_var_in_envi_file) > 0:
+			docker_run_command = command_line
+			for env_var in list(self._env_var_in_envi_file.keys()):
+				docker_run_command = docker_run_command.replace("${" + env_var + "}",
+				                                                self._env_var_in_envi_file[env_var])
+			return docker_run_command
+		else:
+			return command_line
 	
 	def _parse_service(self, service_name, service_params):
 		docker_args = {'name': service_name, 'image': service_params['image']}
 		
 		service_arguments = [args for args in service_params if args in
-		                     ['depends_on', 'links', 'ports', 'expose', 'environment', 'command', 'volumes']]
-		
+		                     list(self._parsers.keys())]
 		for arg in service_arguments:
-			docker_args[arg] = self.parsers[arg](service_params[arg])
+			docker_args[arg] = self._parsers[arg](service_params[arg])
 		return docker_args
+	
+	def _parse_envi_file(self, envi_file):
+		return " --env-file {0} ".format(envi_file[0])
 	
 	def _parse_depends_on(self, deps):
 		return self._to_docker_arg(deps, " --link={0} ")
